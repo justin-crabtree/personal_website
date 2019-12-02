72ce348ccdd50d0481c5cdfe5b57af074164a540
crabtree <justin.kyle.crabtree@gmail.com>
2019-12-02 17:18:31 -0500

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,113 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>Kerri | Responsive Bootstrap 4 Personal Template</title>
+      <meta name="description" content="" />
+      <meta name="keywords" content="" />
+      <meta name="author" content="SRBThemes" />
+
+      <link rel="shortcut icon" href="images/favicon.ico" />
+
+      <!--Bootstrap Css-->
+      <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+
+      <!-- Materialdesign icons Css -->
+      <link href="css/materialdesignicons.min.css" rel="stylesheet" />
+
+      <!-- Mobirise icons Css -->
+      <link href="css/mobiriseicons.css" rel="stylesheet" />
+
+      <!-- Magnific-popup -->
+      <link rel="stylesheet" href="css/magnific-popup.css" />
+
+      <!-- Animate Css -->
+      <link rel="stylesheet" href="css/animate.min.css" />
+
+      <!-- OWL SLIDER -->
+      <link rel="stylesheet" href="css/owl.carousel.css" />
+      <link rel="stylesheet" href="css/owl.theme.css" />
+      <link rel="stylesheet" href="css/owl.transitions.css" />
+
+      <!-- Custom style Css -->
+      <link href="css/style.css" rel="stylesheet" />
+      <link href="css/color/default.css" rel="stylesheet" id="option-color" />
+    </head>
+    <body>
+      <!-- Loader -->
+      <div id="preloader">
+        <div id="status">
+          <div class="spinner">Loading...</div>
+        </div>
+      </div>
+
+      <!-- Start Navbar -->
+      <nav class="navbar navbar-expand-lg fixed-top custom-nav sticky">
+        <div class="container">
+          <a class="navbar-brand pt-0 logo" href="#">
+            <img src="images/logo.png" alt="" class="img-fluid logo-light" />
+            <img src="images/logo-dark.png" alt="" class="img-fluid logo-dark" />
+          </a>
+
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarCollapse"
+            aria-controls="navbarCollapse"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="mdi mdi-menu"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#home">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#about">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#services">Services</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#client">Client</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#work">Portfolio</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#blog">Blog</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#contact">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <!-- End Navbar -->
+
+      <!-- Back To Top -->
+      <a href="#" class="back_top"><i class="mdi mdi-chevron-up"></i></a>
+    </body>
+  </html>
 </template>
 
+<style></style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {};
+  },
+  created: function() {},
+  methods: {}
+};
 </script>
